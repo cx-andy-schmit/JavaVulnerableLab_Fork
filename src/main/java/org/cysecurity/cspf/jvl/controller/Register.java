d3e51c8dc2bfd9adeee6ae5e1aad2b64634e360e
Andy S <46612825+cx-andy-schmit@users.noreply.github.com>
2026-04-08 12:06:50 -0400

    fff

--- src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/Register.java
@@ -83,7 +83,7 @@ public class Register extends HttpServlet {
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
-     * Handles the HTTP <code>GET</code> method. boopsddd
+     * Handles the HTTP <code>GET</code> method. boopsdddddd
      *
      * @param request servlet request
      * @param response servlet response
